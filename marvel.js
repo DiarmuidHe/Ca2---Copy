020eaab4a2628d179ff8bb028133867504824454
DiarmuidHe <s00251204@atu.ie>
2024-12-02 21:07:02 +0000

    pushinng to git

--- marvel.js
+++ marvel.js
@@ -9,22 +9,53 @@ let hashVal = "f780ee3106f11e8941631f9c4b467473";
  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}`;
  showMarvel.innerHTML = "";
 
+// Select the root element in the HTML where we will append our content
+const app = document.getElementById('root');
+
+// Create an image element for the logo and set its source
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+// Create a container div to hold the movie cards, set its class for styling
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+// Append the logo and container to the root element
+
+app.appendChild(container);
+
 async function CallingJson()
 {
     try {
         
         const response = await fetch(url);
         const result = await response.json();
-        result.data.results.forEach((element) => {
-        const info = `
+        result.data.results.forEach((character) => {
+        // const info = `
         
-        <div>${element.name}</div>
-        <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="${element.name}" />
-        <div>${element.id}</div>
-        `;
-        showMarvel.innerHTML += info;
-        });
+        // <div>${element.name}</div>
+        // <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="${element.name}" />
+        // <div>${element.id}</div>
+        // `;
+        // showMarvel.innerHTML += info;
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        // Add title
+        const h1 = document.createElement('h1');
+        h1.textContent = character.name;
+        card.appendChild(h1);
+        // Add movie image
+        const image = document.createElement('img');
+        image.src = character.thumbnail.path + '.' + character.thumbnail.extension;
+        image.setAttribute('alt', `${character.name} Poster`);
+        card.appendChild(image);
+
+ 
+        container.appendChild(card);
         console.log(result);
+        });
+  
     } catch (error) {
         
         console.error(error);

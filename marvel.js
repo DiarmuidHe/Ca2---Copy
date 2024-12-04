cb3ec3638c3d524e5d2689b563cf797aede622c5
DiarmuidHe <s00251204@atu.ie>
2024-12-04 19:30:01 +0000

    Finishing product

--- marvel.js
+++ marvel.js
@@ -6,7 +6,8 @@ let showMarvel = document.getElementById("marvelInfo")
  let publicKey = "22c3f1c78613492efe9236cf821f363e";
 let hashVal = "f780ee3106f11e8941631f9c4b467473";
  const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal]
-const ids =       [1009368,   1009220    ,1009664,1009351,1009189,1009282  ,1009187  ,1009610,1010801,1010338  ]
+ //,1009664,1009351,1009189,1009282  ,1009187  ,1009610,1010801,1010338
+const ids =       [1009368,   1009220,1009664,1009351,1009189,1009282  ,1009187  ,1009610,1010801,1010338]
 const character = ["IronMan","CapAmerica","Thor" ,"hulk" ,"Widow","strange","Panther","Spider","Ant" , "Marvel"]
   //showMarvel.innerHTML = "";
 
@@ -26,10 +27,6 @@ app.appendChild(container);
 
 async function Display()
 {
-
-        
-
-        
     loadingIndicator.style.display = "block";
     for (const id of ids)
     {
@@ -41,61 +38,65 @@ async function Display()
             card.setAttribute('class', 'card');
             const image = document.createElement('img');
             image.src = character.image;
-            image.setAttribute('alt', `${character.name} Poster`);
+            image.setAttribute('alt', `${character.name} icon`);
+            image.setAttribute('class','iconImage');
             card.appendChild(image);
             const h1 = document.createElement('h1');
             h1.textContent = character.name;
             card.appendChild(h1);
-            const button = document.createElement('button');
+            const p = document.createElement('p');
+            p.textContent = character.description;
+            card.appendChild(p);
             
-            button.textContent  = `search`;
-            card.appendChild(button);
-    
+            const button = `<button onclick='ComicDisplay(${JSON.stringify(character.comicInfo)},${id})'>Comics</button>`;
+            card.innerHTML += button;
+            
+
+            const div = `<div class="allComics" id="comic-${id}"></div>`;
+            card.innerHTML += div;
+
+            
             container.appendChild(card);
+            
+
         }
         
     }
     loadingIndicator.style.display = "none";
 
-        
-
-
-    // result.data.results.forEach((character) => 
-    // {
-    //     // const info = `
-        
-    //     // <div>${element.name}</div>
-    //     // <img src="${element.thumbnail.path}.${element.thumbnail.extension}" alt="${element.name}" />
-    //     // <div>${element.id}</div>
-    //     // `;
-    //     // showMarvel.innerHTML += info;
-    //     const card = document.createElement('div');
-    //     card.setAttribute('class', 'card');
-    //     const image = document.createElement('img');
-    //     image.src = character.thumbnail.path + '.' + character.thumbnail.extension;
-    //     image.setAttribute('alt', `${character.name} Poster`);
-    //     card.appendChild(image);
-
-        
-    //     const h1 = document.createElement('h1');
-    //     h1.textContent = character.name;
-    //     card.appendChild(h1);
-        
-
-
-    //     container.appendChild(card);
-    //     console.log(result);
-    //     });
-  
-
 }
 Display();
 
-
+async function ComicDisplay(comicInfo,id)
+{
+    const allComics = document.getElementsByClassName('allComics');
+    for(const comicClass of allComics){
+        comicClass.innerHTML = " ";
+    }
+    for(const element of comicInfo)
+    {
+        const comic = await CallingComicJson(element.resourceURI);
+        console.log(id)
+        
+        const comicHolder = document.getElementById(`comic-${id}`);
+        const image = document.createElement('img');
+        image.src = comic.image;
+        image.setAttribute('alt', `${character.title} Poster`);
+        image.setAttribute('class','comicImage');
+        comicHolder.appendChild(image)
+    }
+    // comicInfo.forEach(element => {
+        
+    // });
+    
+}
 async function CallingJson(id) {
+    
+
+
     try {
         
-        //We need to provide the stock symbol and the region
+        
         const response = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${id}?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}`);
 
         if (!response.ok) {
@@ -103,43 +104,45 @@ async function CallingJson(id) {
         }
 
         const result = await response.json();
-        const character = result.data.results[0]
-        // const scores = (data.finance && data.finance.result && data.finance.result.scores) || null;
-        // if (!scores) {
-        //     throw new Error(`Malformed data for ${symbol}: Missing scores`);
-        // }
-        //If scores.symbol is false, undefined or an empty string set 
-        //symbolName to N/A - this is useful for error handling
+        const character = result.data.results[0];
+
         const name = character.name || "N/A";
         const image = character.thumbnail.path + '.' + character.thumbnail.extension || "N/A";
         const description = character.description || "N/A";
-        return { name, image};
+        
+        const comicInfo = character.comics.items;
+        
+        return { name, image, description, comicInfo};
     } catch (error) {
         console.error(`Error fetching data for ${id}:`, error.message);
-        return null; // Graceful handling of errors
+        return null; 
+    }
+}
+async function CallingComicJson(comicUrl) 
+{
+    try {
+        
+
+        const response = await fetch(`${comicUrl}?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}`);
+
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error ${response.status}: Unable to fetch data for ${id}`);
+        }
+
+        const result = await response.json();
+        
+        const comic = result.data.results[0];
+        const title = comic.title || "N/A";
+        const image = comic.thumbnail.path + '.' + comic.thumbnail.extension || "N/A";
+
+        console.log(result);
+        return {title, image };
+    } catch (error) {
+        console.error(`Error fetching data for ${comicUrl}:`, error.message);
+        return null; 
     }
 }
 
-// async function renderStocks() {
-//     stockTableBody.innerHTML = ""; // Clear existing rows
-//     loadingIndicator.style.display = "block"; // Show loading indicator
 
-//     for (const symbol of stocks) {
-//         const stock = await fetchStockData(symbol);
-//         if (stock) {
-//             const row = document.createElement("tr");
-//             row.innerHTML = `
-//                 <td>${stock.symbolName}</td>
-//                 <td>${stock.sector}</td>
-//                 <td>${stock.overallScore}</td>
-//                 <td>${stock.valuationDescription}</td>
-//                 <td>${stock.hiringDirection}</td>
-//                 <td>${stock.dividendYield}</td>
-//             `;
-//             stockTableBody.appendChild(row);
-//         }
-//     }
-
-//     loadingIndicator.style.display = "none"; // Hide loading indicator
-// }
 
